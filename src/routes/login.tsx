a95cea7251ba2a15cec154698ee27df3061dfe6a
Adam Fenwick <afenwickdev@gmail.com>
2025-06-17 12:11:53 +0100

    Frontend push

---
+++ src/routes/login.tsx
@@ -0,0 +1,46 @@
+import { useNavigate } from '@tanstack/react-router'
+
+export const Route = createFileRoute({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  const navigate = useNavigate()
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    fetch('http://localhost:3000/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    })
+    .then(response => {
+        if (!response.ok) throw new Error('Login failed');
+        return response.json();
+      })
+      .then(data => {
+        console.log('Login successful:', data);
+        navigate({ to: '/' });
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+  return (
+    <div>
+      <form method="POST" onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" id="email" />
+        <label htmlFor="password">Register</label>
+        <input type="password" name="password" id="password" />
+        <button>Continue</button>
+      </form>
+    </div>
+  );
+}
+
---
+++ src/routes/register.tsx
@@ -0,0 +1,42 @@
+export const Route = createFileRoute({
+  component: RouteComponent,
+})
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const email = event.target.email.value;
+  const password = event.target.password.value;
+  console.log('EMAIL + PASSWORD: ', email, password);
+
+  fetch('http://localhost:3000/auth/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password })
+  })
+  .then(response => {
+      if (!response.ok) throw new Error('Registration failed');
+      return response.json();
+    })
+    .then(data => {
+      console.log('Registration successful:', data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+function RouteComponent() {
+  return (
+    <div>
+      <form method="POST" onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" id="email" />
+        <label htmlFor="password">Register</label>
+        <input type="password" name="password" id="password" />
+        <button>Continue</button>
+      </form>
+    </div>
+  );
+}
